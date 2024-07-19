7d259f136f7126086c6f1ce09c036ed7a56911bf
Pavlo_Sernetskyi <74708976+PavloSernetskyi@users.noreply.github.com>
2024-07-19 11:13:36 -0700

    initializing java spring boot project

---
+++ src/main/java/qwasar/investmens_fund/InvestmensFundApplication.java
@@ -0,0 +1,13 @@
+package qwasar.investmens_fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmensFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmensFundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qwasar/investmens_fund/InvestmensFundApplicationTests.java
@@ -0,0 +1,13 @@
+package qwasar.investmens_fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmensFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
