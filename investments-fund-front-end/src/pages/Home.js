de2366bee1431081accd312a0698cbd538c4c951
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2024-08-14 15:15:11 -0700

    added filter to sort investments by amount invested

--- investments-fund-front-end/src/pages/Home.js
+++ investments-fund-front-end/src/pages/Home.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
 
 // create obj to store investments info. axios part.
-    const [investments, setUsers] = useState([])
+    const [investments, setInvestments] = useState([])
 // now every time page will be loaded it will load investments info and we need to use useEffect for that.
     useEffect(() =>{
       console.log("Code with pavlo")
@@ -15,7 +15,7 @@ export default function Home() {
       const result = await axios.get("http://localhost:8080/investments")
       console.log(result.data);
       // to dipslay in table..
-      setUsers(result.data);
+      setInvestments(result.data);
     }
 
     const deleteInvestment= async (id)=>{
@@ -23,21 +23,29 @@ export default function Home() {
       loadInvestments();
     }
 
+  // Function to sort investments by amount invested in descending order
+  const sortInvestmentsByAmount = () => {
+    const sorted = [...investments].sort((a, b) => b.amountInvested - a.amountInvested);
+    setInvestments(sorted);
+  };
+
 
   return (
     <div className='container'>
     <div className='p-4'>
+    <h1>Investments Fund</h1>
+    <button className="btn btn-primary mb-3" onClick={sortInvestmentsByAmount}>Sort by Amount Invested</button>
       <table className="table border shadow">
         <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">Investment name</th>
+          <th scope="col">Unique ID</th>
+          <th scope="col">Investment Name</th>
           <th scope="col">Description</th>
           <th scope="col">Amount Invested</th>
 
         </tr>
         </thead>
-        {/* use user id  to have same id as in database. or use index and index +1 */}
+        {/* use investments.id to have same id as in database. or use index and index +1 to display investments from 1 to nth... */}
         { <tbody>
         {investments.map((investment, index) => (<tr key={investment.id}>
               <th scope="row">{investment.id }</th>
